8896cfaacd74dd9b84b142c77aaf03efc64a8818
Frank  D'Angelo <08fdange@gmail.com>
2020-02-14 17:27:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  string = ""
+  x = array.length
+  if array.length == 1 
+    string << array.join
+  elsif array.length == 2
+    string << array.join(" and ")
+  elsif array.length >= 3
+    string << array.join("")
+  return string
 end
